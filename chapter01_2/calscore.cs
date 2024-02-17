d5653ecf2051a852c13827c3f5f3e96a31c6055d
Saravut hungsakunrat <66111310@dpu.ac.th>
2024-02-17 10:35:21 +0700

    test3

---
+++ chapter01_2/calscore.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace chapter01_2
+{
+	public class calscore
+	{
+        public static void Main(string[] agrs)
+        {
+			int student;
+			int[] coursework;
+			int[] midterm;
+			int[] final;
+			int[] sumtotal;
+
+			Console.Write("Pleas enter the number of student: = ");
+			student = Convert.ToInt32(Console.ReadLine());
+
+            coursework = new int[student];
+            midterm = new int[student];
+            final = new int[student];
+            sumtotal = new int[student];
+
+            for (int i =0; i < student; i++)
+			{
+                Console.Write("Pleas enter coursework of student #{0} : = ",i+1);
+				coursework[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Pleas enter midter, of student #{0} : = ", i + 1);
+				midterm[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Pleas enter final of student #{0} : = ", i + 1);
+				final[i] = Convert.ToInt32(Console.ReadLine());
+				sumtotal[i] = coursework[i] + midterm[i] + final[i];
+				Console.WriteLine("-------------------------------------------");
+
+            }
+			int j  = 1;
+			foreach (int sumscore in sumtotal)
+			{
+				Console.WriteLine("Total marks of student#{0} = {1}", j, sumscore);
+			}
+
+
+
+		}
+	}
+}
+
--- chapter01_2/test1.cs
+++ chapter01_2/test1.cs
@@ -3,24 +3,24 @@
 //{
 //	public class test1
 //	{
-//        public static void Main(string[] agrs)
-//        {
+//		public static void Main(string[] agrs)
+//		{
 //			int allsum = 0;
 //			while (true)
 //			{
 //				int number;
-				
-//				Console.Write("Please enter number : ");
-//				number= Convert.ToInt32(Console.ReadLine());
-       
-//				if (number == 0)
-//				{ Console.Write("SUM = {0}", allsum);break; }
-//				allsum = allsum + number;
-				
-//            }
-			
 
-//        }
+//				Console.Write("Please enter number : ");
+//				number = Convert.ToInt32(Console.ReadLine());
+
+//				if (number <= 0)
+//				{ Console.WriteLine("SUM = {0}", allsum); break; }
+//				allsum = allsum + number;
+
+//			}
+
+
+//		}
 //	}
 //}
 
